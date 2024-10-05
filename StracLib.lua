86b4fb7f89ac597aa09e07aa2daab65877b16f8a
Tensei Lawden <148848858+Cortzalno666@users.noreply.github.com>
2024-10-05 12:59:21 +0600

    Update and rename AimerHub [StracLib].lua.txt to StracLib.lua

--- StracLib.lua
+++ StracLib.lua
@@ -1,6 +1,6 @@
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Welcome", -- Required
-	Text = "Made Using StracLib", -- Required
+	Text = "Welcome!", -- Required
 })
 
 libary = {}
@@ -132,7 +132,7 @@ function libary:CreateMain(text)
 	Credits.Position = UDim2.new(0.022, 0, 0.835390925, 0)
 	Credits.Size = UDim2.new(0, 118, 0, 40)
 	Credits.Font = Enum.Font.SourceSans
-	Credits.Text = "Made By NectoVerse Development"
+	Credits.Text = "UI Library made By NectoVerse Development"
 	Credits.TextColor3 = Color3.fromRGB(255, 255, 255)
 	Credits.TextScaled = true
 	Credits.TextSize = 14.000
